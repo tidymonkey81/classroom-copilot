cbdd8c82d7092cf35e7f8f6828f6721f5aa568df
kevlarmonk <kcar@kevlarai.com>
2024-06-03 22:22:55 +0100

    updates

--- frontend/src/pages/services/llmService.ts
+++ frontend/src/pages/services/llmService.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const sendPrompt = async (data: { model: string, prompt: string, temperature: number }, backendUrl: string) => {
+export const sendPrompt = async (data: { model: string, question: string, temperature: number }, backendUrl: string) => {
   const response = await axios.post(`${backendUrl}/llm/ollama_text_prompt`, data);
   return response.data;
 };

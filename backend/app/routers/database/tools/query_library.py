7527ed04fa29388cd00d12e7ddb112dfb0735a06
kevlarmonk <kcar@kevlarai.com>
2024-06-23 13:58:29 +0100

    minor changes

--- backend/app/routers/database/tools/query_library.py
+++ backend/app/routers/database/tools/query_library.py
@@ -1,5 +1,10 @@
 def create_database(db_name):
         f"CREATE DATABASE `{db_name}` IF NOT EXISTS"
+        
+def delete_database(db_name):
+        f":USE system"
+        f"STOP DATABASE {db_name}"
+        f"DROP DATABASE {db_name}"
 
 show_constraints = "SHOW CONSTRAINTS"
 

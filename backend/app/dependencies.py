12579d64ec3526b3643137f5c94a5ef967136e99
kevlarmonk <kcar@kevlarai.com>
2024-07-11 06:25:03 +0100

    updates

--- backend/app/dependencies.py
+++ backend/app/dependencies.py
@@ -3,10 +3,11 @@ from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError, jwt
 from datetime import datetime, timedelta
 from typing import Optional
+from dotenv import load_dotenv
+import os
 
-# to get a string like this run:
-# openssl rand -hex 32
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+load_dotenv()
+SECRET_KEY = os.getenv("FASTAPI_SECRET_KEY")
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
